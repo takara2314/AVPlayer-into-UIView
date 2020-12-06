791279b7bc858d0884ae86012267c94ec49f2669
takara2314 <takara2314_TK@outlook.jp>
2020-12-06 18:12:25 +0900

    First commit

--- practice2/ViewController.swift
+++ practice2/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var dorabel: UILabel!
+    @IBOutlet weak var doraview: UIView!
+    
+    let controller = AVPlayerViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        dorabel.text = "懐かしいね"
+        doraview.backgroundColor = UIColor.blue
+        
+        let videoPath = Bundle.main.path(
+            forResource: "count2020",
+            ofType: "mp4"
+        )!
+        controller.showsPlaybackControls = false
+        controller.player = AVPlayer(
+            url: URL(fileURLWithPath: videoPath)
+        )
+        
+        doraview.addSubview(controller.view)
+        controller.view.frame = doraview.bounds
+        controller.player?.play()
     }
-
-
 }
 
